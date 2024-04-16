bcd9c0957c3e36079329a55dbd12e63688270bde
Artemii <63139705+ArtemiiK@users.noreply.github.com>
2024-04-16 17:30:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("Wedding/index.html")
 
 #if __name__ == '__main__':
     #app.run(port=5000)
